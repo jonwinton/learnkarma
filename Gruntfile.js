1f1927e7776a16bd1ce1234122c4fa7a9c81355d
Jon Winton <winton.jon@gmail.com>
2015-07-22 20:37:37 -0400

    Forgot to add in the pkg property :/

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,27 +2,28 @@
 
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    karma: {
-        testScripts: {
-            options: {
-                singleRun: true,
-                configFile: 'karma.conf.js',
-                basePath: 'src/',
-                coverageReporter: {
-                    type: 'html',
-                    // path is relative to basePath
-                    // adjust to the best location for your project
-                    // and backend
-                    dir: '../build-reports/'
-                }
-            },
-            reporters: ['progress', 'coverage']
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        karma: {
+            testScripts: {
+                options: {
+                    singleRun: true,
+                    configFile: 'karma.conf.js',
+                    basePath: 'src/',
+                    coverageReporter: {
+                        type: 'html',
+                        // path is relative to basePath
+                        // adjust to the best location for your project
+                        // and backend
+                        dir: '../build-reports/'
+                    }
+                },
+                reporters: ['progress', 'coverage']
+            }
         }
-    }
-  });
+    });
 
-
-  grunt.registerTask('default', ['karma']);
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.registerTask('default', ['karma']);
 
 };
