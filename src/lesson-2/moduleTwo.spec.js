0d0d0f22f32e35c246090cf8958b9f9e64621c92
Jon Winton <winton.jon@gmail.com>
2015-07-22 23:22:16 -0400

    Added testing for instantiation of Demoview from module two

--- src/lesson-2/moduleTwo.spec.js
+++ src/lesson-2/moduleTwo.spec.js
@@ -1,11 +1,16 @@
 /// <reference path="moduleTwo.js">
+/// <reference path="DemoView.js">
 'use strict';
 
 describe('Test class', function() {
 
     it('calls init when created', function() {
+        var TESTING = window.TESTING || {};
+        var Test = TESTING.Test;
         // setup a spy on the Test.init method to veryify that it was called
         spyOn(Test.prototype, 'init');
+
+
 
         // perform the code to be tested
         var test = new Test();
@@ -14,6 +19,19 @@ describe('Test class', function() {
         expect(Test.prototype.init).toHaveBeenCalled();
 
         expect(Test.prototype.init.calls.count()).toEqual(1);
+    });
+
+    it('creates a demo view', function() {
+        var TESTING = window.TESTING || {};
+        var Test = TESTING.Test;
+        var DemoView = TESTING.DemoView;
+
+        // perform the code to be tested
+        var test = new Test();
+
+        console.log(test);
+
+        expect(test.demo instanceof DemoView).toEqual(true);
     });
 
 });
