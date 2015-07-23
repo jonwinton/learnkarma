a7c1036f29b19418ba766bde0877a80d13787394
Jon Winton <winton.jon@gmail.com>
2015-07-22 20:49:50 -0400

    Wrote first test! It passed!

--- src/lesson-1/firstModule.js
+++ src/lesson-1/firstModule.js
@@ -2,5 +2,12 @@
 
 // My first module to test!
 var firstModule = (function(){
-  
-})();
+    var merge = function(string1, string2) {
+        return string1 + string2;
+    };
+
+
+  return {
+      merge: merge
+  };
+}());
--- src/lesson-1/firstModule.spec.js
+++ src/lesson-1/firstModule.spec.js
@@ -1,6 +1,17 @@
 /// <reference path="firstModule.js">
 'use strict';
 
-describe('My firstModule test suite', function(){
+describe('My firstModule test suite', function() {
+    it("To concat two strings", function() {
+        // Arrange
+        var string1 = 'string1';
+        var string2 = 'string2'
+        var expected = 'string1string2';
 
+        // Act
+        var result = firstModule.merge(string1, string2);
+
+        // Assert
+        expect(result).toBe(expected);
+    });
 });
